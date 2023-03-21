00fb6af40de82e049c20acdfd5d664c54cb2f02d
Nicholas Gonzalez <thisnickg194@gmail.com>
2023-03-21 15:45:43 -0500

    form works for updating skill info

--- src/EditSkill.jsx
+++ src/EditSkill.jsx
@@ -1,50 +1,138 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { JournalIndex } from "./JournalIndex";
 
 export function EditSkill() {
   const [skill, setSkill] = useState({});
   const params = useParams();
 
-  const handleShowUser = () => {
+  const handleShowSkill = () => {
     axios.get(`http://localhost:3000/skills/${params.id}.json`).then((response) => {
       console.log(response.data);
       setSkill(response.data);
     });
   };
 
-  useEffect(handleShowUser, []);
+  const handleUpdateSkillInfo = (id, params) => {
+    console.log(id);
+    axios.patch(`http://localhost:3000/skills/${id}.json`, params).then((response) => {
+      const updatedPost = response.data;
+      setSkill(response.data);
+    });
+  };
+
+  const handleSkillSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+
+    handleUpdateSkillInfo(skill.id, params);
+  };
+
+  useEffect(handleShowSkill, []);
 
   return (
-    <form>
-      <div className="form-group">
-        <label for="skillNameInput">Journal Name</label>
-        <input name="name" type="text" className="form-control" id="skillNameInput" defaultValue={skill.name} />
-      </div>
-      <div className="form-group">
-        <label for="skillDescriptionInput">Description</label>
-        <input
-          name="description"
-          type="text"
-          className="form-control"
-          id="skillDescriptionInput"
-          defaultValue={skill.description}
-        />
-      </div>
-      <div className="form-group">
-        <label for="skillStartInput">Start Date</label>
-        {/* TODO figure out how to turn type into date */}
-        <input name="start" type="text" className="form-control" id="skillStartInput" defaultValue={skill.start} />
-      </div>
-      <div className="form-group">
-        <label for="skillEndInput">End Date</label>
-        {/* TODO figure out how to turn type into date */}
-        <input name="end" type="text" className="form-control" id="skillEndInput" defaultValue={skill.end} />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSkillSubmit}>
+        <div className="form-group">
+          <label htmlFor="skillNameInput">Journal Name</label>
+          <input name="name" type="text" className="form-control" id="skillNameInput" defaultValue={skill.name} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="skillDescriptionInput">Description</label>
+          <input
+            name="description"
+            type="text"
+            className="form-control"
+            id="skillDescriptionInput"
+            defaultValue={skill.description}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="skillStartInput">Start Date</label>
+          {/* TODO figure out how to turn type into date */}
+          <input name="start" type="text" className="form-control" id="skillStartInput" defaultValue={skill.start} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="skillEndInput">End Date</label>
+          {/* TODO figure out how to turn type into date */}
+          <input name="end" type="text" className="form-control" id="skillEndInput" defaultValue={skill.end} />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+      {skill.resources?.map((resource) => (
+        <form key={resource.id}>
+          <div className="form-group">
+            <label htmlFor="resourceNameInput">Resource Name</label>
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="resourceNameInput"
+              defaultValue={resource.name}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resourceDescriptionInput">Description</label>
+            <input
+              name="description"
+              type="text"
+              className="form-control"
+              id="resourceDescriptionInput"
+              defaultValue={resource.description}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resourceStartInput">Start Date</label>
+            {/* TODO figure out how to turn type into date */}
+            <input
+              name="start"
+              type="text"
+              className="form-control"
+              id="resourceStartInput"
+              defaultValue={resource.start}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resourceEndInput">End Date</label>
+            {/* TODO figure out how to turn type into date */}
+            <input name="end" type="text" className="form-control" id="resourceEndInput" defaultValue={resource.end} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resourceURLInput">URL</label>
+            <input name="url" type="text" className="form-control" id="resourceURLInput" defaultValue={resource.url} />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      ))}
+      {skill.projects?.map((project) => (
+        <form key={project.id}>
+          <div className="form-group">
+            <label htmlFor="projectNameInput">Project Name</label>
+            <input name="name" type="text" className="form-control" id="projectNameInput" defaultValue={project.name} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="projectDescriptionInput">Description</label>
+            <input
+              name="description"
+              type="text"
+              className="form-control"
+              id="projectDescriptionInput"
+              defaultValue={project.description}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="projectURLInput">URL</label>
+            <input name="url" type="text" className="form-control" id="projectURLInput" defaultValue={project.url} />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      ))}
+    </div>
   );
 }
--- src/JournalIndex.jsx
+++ src/JournalIndex.jsx
@@ -5,7 +5,10 @@ export function JournalIndex(props) {
       {props.skills.map((skill) => (
         <div key={skill.id} className="container">
           <hr />
-          <h1>{skill.name}</h1>
+          <h2>{skill.name}</h2>
+          <p>{skill.description}</p>
+          <p>Start Date: {skill.start}</p>
+          <p>End Date: {skill.end}</p>
           {skill.user ? (
             <a href={`/profile/${skill.user_id}`}>
               <h3>{skill.user}</h3>
@@ -24,7 +27,9 @@ export function JournalIndex(props) {
             <div key={resource.id}>
               <h4>{resource.name}</h4>
               <p>{resource.description}</p>
-              <p>{resource.url}</p>
+              <p>Started: {resource.start}</p>
+              <p>Ended: {resource.end}</p>
+              <p>Source: {resource.url}</p>
             </div>
           ))}
           {skill.projects.map((project) => (
